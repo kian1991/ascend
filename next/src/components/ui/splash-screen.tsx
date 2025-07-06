d836dbfc95e1b75e51ed10683366ab470bb52dba
kian1991 <kianluetke@gmail.com>
2025-07-06 06:25:46 +0200

    refactor:fixed splash screen

--- next/src/components/ui/splash-screen.tsx
+++ next/src/components/ui/splash-screen.tsx
@@ -4,36 +4,38 @@ import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
 export const SplashScreen = () => {
-    const router = useRouter();
+  const router = useRouter();
 
-    useEffect(() => {
-        // const timeout = setTimeout(() => {
-        //     router.push('/login');
-        // }, 4200);
-        // return () => {
-        //     clearTimeout(timeout);
-        // };
-    }, [router]);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      router.push('/login');
+    }, 4200);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [router]);
 
-    return (
-        <div className="flex flex-col items-center relative justify-center">
-            <motion.div
-                initial={{ bottom: '-100%' }}
-                animate={{ bottom: '0%' }}
-                transition={{ duration: 1, ease: 'easeInOut' }}
-                className="absolute inset-0 bg-gradient-to-b from-primary to-secondary"
-            />
+  return (
+    <div className='flex flex-col items-center relative h-screen justify-center'>
+      <motion.div
+        initial={{ bottom: '-100%' }}
+        animate={{ bottom: '0%' }}
+        transition={{ duration: 1, ease: 'easeInOut' }}
+        className='absolute inset-0 bg-gradient-to-b from-amber-100 to-sky-300'
+      />
 
-            <motion.img
-                src="/img/ascend.png"
-                alt="Ascend Logo"
-                className="w-64 absolute"
-                initial={{ opacity: 0, y: 100 }}
-                animate={{ opacity: [0, 1, 0], y: [100, 0, -100] }}
-                transition={{ duration: 6, ease: 'easeInOut' }}
-            />
+      <motion.img
+        src='/img/ascend.png'
+        alt='Ascend Logo'
+        className='w-64 absolute'
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: [0, 1, 0], y: [100, 0, -100] }}
+        transition={{ duration: 6, ease: 'easeInOut' }}
+      />
 
-            <h1 className="text-3xl font-bold text-center absolute bottom-10 text-base-content">Welcome.</h1>
-        </div>
-    );
+      <h1 className='text-3xl font-bold text-center absolute bottom-10 text-base-content'>
+        Welcome.
+      </h1>
+    </div>
+  );
 };
